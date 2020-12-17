0f86ef9051dc7dea09c44d1a6b87234c6821f7f4
srayas <50865098+srayas@users.noreply.github.com>
2020-12-17 20:46:41 +0530

    Adding lisetners to button

---
+++ index.js
@@ -0,0 +1,57 @@
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick); 
+  function handleClick() {
+    var butonCalled = this.innerHTML;
+    makeSound(butonCalled);
+    buttonAnimation(butonCalled);
+  }
+}
+document.addEventListener('keypress', function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+// catching the click and Key press simply capturing the event triggered.
+function makeSound(key){
+    switch (key) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+        case "a":
+          var audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+        case "s":
+          var audio = new Audio("sounds/tom-3.mp3");
+          audio.play();
+          break;
+        case "d":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+          break;
+        case "j":
+          var audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+        case "k":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+        case "l":
+          var audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+        default:
+          break;
+      }
+
+}
+
+function buttonAnimation(key){
+
+   var activeButtoun = document.querySelector("."+key)
+    activeButtoun.classList.add("pressed");
+   setTimeout(function(){
+    activeButtoun.classList.remove("pressed");
+   },100);
+}
